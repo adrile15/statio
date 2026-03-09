94413e7aef285ac46f6664c2399ecadb81d3450c
adrile15 <adrile1501@gmail.com>
2026-03-09 17:09:53 +0100

    Primeros pasos web app

---
+++ backend/statio-api/src/main/java/com/statio/api/StatioApiApplication.java
@@ -0,0 +1,13 @@
+package com.statio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatioApiApplication.class, args);
+	}
+
+}
---
+++ backend/statio-api/src/test/java/com/statio/api/StatioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
