33f33d63be6eb97057d77db8cda5fc98ccded256
adrile15 <adrile1501@gmail.com>
2026-05-16 09:16:18 +0200

    fix uploads

--- backend/statio-api/src/main/java/com/statio/api/controller/ImageController.java
+++ backend/statio-api/src/main/java/com/statio/api/controller/ImageController.java
@@ -1,23 +1,22 @@
 package com.statio.api.controller;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.UUID;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/images")
 @CrossOrigin
 public class ImageController {
 
-    private final String UPLOAD_DIR =
-            System.getProperty("user.dir") + "/uploads/";
+    @Autowired
+    private Cloudinary cloudinary;
 
     @PostMapping("/upload")
     public ResponseEntity<String> uploadImage(
@@ -25,25 +24,15 @@ public class ImageController {
 
         try {
 
-            File uploadDir = new File(UPLOAD_DIR);
+            Map uploadResult = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.emptyMap()
+            );
 
-            if(!uploadDir.exists()){
-                uploadDir.mkdirs();
-            }
+            String imageUrl =
+                    uploadResult.get("secure_url").toString();
 
-            String extension =
-                    StringUtils.getFilenameExtension(
-                            file.getOriginalFilename()
-                    );
-
-            String fileName =
-                    UUID.randomUUID() + "." + extension;
-
-            Path path = Paths.get(UPLOAD_DIR + fileName);
-
-            Files.write(path, file.getBytes());
-
-            return ResponseEntity.ok("/uploads/" + fileName);
+            return ResponseEntity.ok(imageUrl);
 
         } catch (Exception e){
 
